16b3155e9c5fa1f93b54b2f9637b7f5fa66e1293
AziaRae <github.xsn9j@akayashi.slmail.me>
2025-10-10 17:31:31 +0800

    - partially completed init_grid()
    created a function that generates random coordinates for all objects. the coordinates do not conflict with each other

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 # (i, j) will denote the row (top-bottom) and column (left-right) of Laro
 
 import sys
+import random
 
+# TEST
+# create an r x c grid
+r, c = map(int, input().split())
+
+# initialization of characters in the grid will follow the same order here
 characters = {
-    "tree": "T",
     "empty": ".",
+    "tree": "T",
     "mushroom": "+",
     "water": "~",
     "rock": "R",
@@ -14,15 +20,106 @@ characters = {
     "flamethrower": "*",
 }
 
-# the quantity per object will be randomized
-global num_mushrooms, num_trees, num_fire, num_water, num_pickaxe, num_rock
-num_mushrooms = 1
+# the quantity of the mushrooms will be randomized
+total_characters = r * c
+total_free = total_characters - (2 * (r + c) - 4)
 collected_mushrooms = 0
+
+num_laro = 1
+num_mushrooms = random.randint(1, max((r * c) // 10, 1))
+num_trees = random.randint(0, (total_free - num_laro) // 2)
+num_water = random.randint(0, (total_free - num_mushrooms - num_trees) // 4)
+num_rock = random.randint(0, (total_free - num_mushrooms - num_water - num_trees) // 4)
+num_pickaxe = random.randint(0, (total_free - num_mushrooms - num_water - num_trees - num_rock) // 2)
+num_flamethrower = random.randint(0, (total_free - num_mushrooms - num_water - num_trees - num_rock - num_pickaxe) // 2)
 
 winning_message = "olanap lodicakes"
 
-# initialize the grid
-grid = []
+
+def is_edge(y: int, x: int) -> bool: 
+    return (y in (0, r - 1) and 0 <= x < c) or (x in (0, c - 1) and 0 <= y < c)
+
+def is_inside(y: int, x: int) -> bool:
+    return 0 <= y < r and 0 <= x < c
+
+def init_grid():
+    # initialize the grid
+    grid = [list("T" if is_edge(R,C) else "." for C in range(c)) for R in range(r)]
+    coords = []
+    
+    def gen_coordinates_for_item(quantity: int, coordinates = None, coords = coords):
+        
+        if coordinates == None:
+            coordinates = []
+        
+        if len(coordinates) == quantity:
+            return coordinates
+        
+        coordinate = [random.randint(1, r - 2), random.randint(1, c - 2)]
+        
+        if coordinate in coords:
+            return gen_coordinates_for_item(quantity, coordinates)
+        else:
+            coordinates.append(coordinate)
+            return gen_coordinates_for_item(quantity, coordinates)
+        
+    laro = gen_coordinates_for_item(num_laro)
+    coords.extend(laro)
+    
+    mushrooms = gen_coordinates_for_item(num_mushrooms)
+    coords.extend(mushrooms)
+    
+    trees = gen_coordinates_for_item(num_trees)
+    coords.extend(trees)
+    
+    waters = gen_coordinates_for_item(num_water)
+    coords.extend(waters)
+    
+    rocks = gen_coordinates_for_item(num_rock)
+    coords.extend(rocks)
+    
+    pickaxes = gen_coordinates_for_item(num_pickaxe)
+    coords.extend(pickaxes)
+    
+    flamethrowers = gen_coordinates_for_item(num_flamethrower)
+    coords.extend(flamethrowers)
+    
+    print(f"coords = {coords}")
+    
+    # TODO: Convert these into a function for abstraction
+    for l in laro:
+        y, x = l
+        grid[y][x] = characters["laro"]
+    
+    for mushroom in mushrooms:
+        y, x = mushroom
+        grid[y][x] = characters["mushroom"]
+        
+    for tree in trees:
+        y, x = tree
+        grid[y][x] = characters["tree"]
+        
+    for water in waters:
+        y, x = water
+        grid[y][x] = characters["water"]
+        
+    for rock in rocks:
+        y, x = rock
+        grid[y][x] = characters["rock"]
+        
+    for pickaxe in pickaxes:
+        y, x = pickaxe
+        grid[y][x] = characters["pickaxe"]
+        
+    for flamethrower in flamethrowers:
+        y, x = flamethrower
+        grid[y][x] = characters["flamethrower"]
+            
+    return grid
+
+grid = init_grid()
+for subgrid in grid:
+    print(subgrid)
 
 
 def ending_screen() -> None:
